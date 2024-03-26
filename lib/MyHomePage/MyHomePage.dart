f4435e50872901fe3e34e24f19a0f20e91a483d6
cardacaptor <rajopadhyayasrijita14@gmail.com>
2024-03-26 18:03:44 +0545

    bug solved

--- lib/MyHomePage/MyHomePage.dart
+++ lib/MyHomePage/MyHomePage.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -5,6 +6,7 @@ import 'package:ghumfir_f/MyHomePage/Recommenadation/landings/Explore.dart';
 import 'package:ghumfir_f/MyHomePage/Recommenadation/landings/Recommenadation.dart';
 import 'package:ghumfir_f/MyHomePage/Recommenadation/landings/Trending.dart';
 import 'package:ghumfir_f/MyHomePage/SearchBar/SearchPart.dart';
+import 'package:ghumfir_f/api.dart';
 
 class MyHomePage extends StatefulWidget {
   final bool isOpen;
@@ -17,6 +19,23 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int currentIndex = 0;
+  bool isForYou = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      isForYou = Api.token != null;
+    });
+    Api.tokenListeners.add((_, String? currToken) {
+      if (mounted) {
+        setState(() {
+          isForYou = currToken != null;
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,48 +73,60 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Column(
                           children: [
                             Container(height: 20),
-                            Column(
-                              children: ["Explore", "For you", "Trending"]
-                                  .asMap()
-                                  .entries
-                                  .map(
-                                    (element) => GestureDetector(
-                                      onTap: () => setState(
-                                          () => currentIndex = element.key),
-                                      child: Container(
-                                        height: 50,
-                                        decoration: BoxDecoration(
-                                          color: element.key == currentIndex
-                                              ? Colors.green
-                                              : null,
-                                          borderRadius:
-                                              BorderRadius.circular(12),
-                                        ),
-                                        alignment: Alignment.centerLeft,
-                                        padding: const EdgeInsets.symmetric(
-                                            horizontal: 12, vertical: 12),
-                                        child: Text(
-                                          element.value,
-                                          style: const TextStyle(
-                                              color: Colors.white),
+                            Builder(builder: (context) {
+                              List<String> navTabBars = [
+                                "Explore",
+                                if (isForYou) "For you",
+                                "Trending"
+                              ];
+                              return Column(
+                                children: navTabBars
+                                    .asMap()
+                                    .entries
+                                    .map(
+                                      (element) => GestureDetector(
+                                        onTap: () => setState(
+                                            () => currentIndex = element.key),
+                                        child: Container(
+                                          height: 50,
+                                          decoration: BoxDecoration(
+                                            color: element.key ==
+                                                    min(currentIndex,
+                                                        navTabBars.length - 1)
+                                                ? Colors.green
+                                                : null,
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                          alignment: Alignment.centerLeft,
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 12, vertical: 12),
+                                          child: Text(
+                                            element.value,
+                                            style: const TextStyle(
+                                                color: Colors.white),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  )
-                                  .toList(),
-                            ),
+                                    )
+                                    .toList(),
+                              );
+                            }),
                           ],
                         ),
                       ),
                       Expanded(
-                        child: IndexedStack(
-                          index: currentIndex,
-                          children: const [
-                            Explore(),
-                            Recommenadation(),
-                            Trending(),
-                          ],
-                        ),
+                        child: Builder(builder: (context) {
+                          List<Widget> widgets = [
+                            const Explore(),
+                            if (isForYou) const Recommenadation(),
+                            const Trending(),
+                          ];
+                          return IndexedStack(
+                            index: min(currentIndex, widgets.length - 1),
+                            children: widgets,
+                          );
+                        }),
                       ),
                     ],
                   ),
